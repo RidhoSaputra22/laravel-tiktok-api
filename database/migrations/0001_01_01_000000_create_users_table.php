834cb54d291590e67e9d0d23af490ac6c2522c21
ridho-code <60309031+ridho-code@users.noreply.github.com>
2025-06-26 06:47:33 +0800

    -

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id()->primary()->index();
-            $table->string('nama');
-            $table->date('tanggal_lahir');
-            $table->string('gender');
-            $table->string('email')->unique();
+            $table->string('nama')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('email')->nullable();
             $table->string('phone')->unique();
             $table->string('password');
             $table->string('photo')->nullable();
